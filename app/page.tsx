d278e0e4062b1f48caa39d524880ec2ab2ff6e3a
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2024-01-30 18:27:24 +0900

    inputのvalue取得

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,20 @@
 import Todo from "./components/Todo";
 import useSWR from "swr";
 import { TodoType } from "./types";
+import { FormEvent, useRef } from "react";
 
 async function fetcher(key: string) {
   return fetch(key).then((res) => res.json());
 }
 
 export default function Home() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const { data, isLoading, error } = useSWR("http://localhost/api/todos", fetcher);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+  }
 
   return (
     <div
@@ -18,7 +25,7 @@ export default function Home() {
       <div className="px-4 py-2">
         <h1 className="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
       </div>
-      <form className="w-full max-w-sm mx-auto px-4 py-2">
+      <form className="w-full max-w-sm mx-auto px-4 py-2" onSubmit={handleSubmit}>
         <div className="flex items-center border-b-2 border-teal-500 py-2">
           <input
             className="appearance-none bg-transparent
@@ -26,6 +33,7 @@ export default function Home() {
       focus:outline-none"
             type="text"
             placeholder="Add a task"
+            ref={inputRef}
           />
           <button
             className="duration-150 flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
